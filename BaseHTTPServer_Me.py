48e4df6a97fc707c8655dd46ea9f62d568889722
soarpenguin <soarpenguin@gmail.com>
2014-04-16 22:57:44 +0800

    BaseHTTPServer add POST method.

--- BaseHTTPServer_Me.py
+++ BaseHTTPServer_Me.py
@@ -2,6 +2,40 @@
 
 from BaseHTTPServer import BaseHTTPRequestHandler
 import urlparse
+import cgi
+
+class PostHandler(BaseHTTPRequestHandler):
+    
+    def do_POST(self):
+        # Parse the form data posted
+        form = cgi.FieldStorage(
+                fp = self.rfile,
+                headers = self.headers,
+                environ = {'REQUEST_METHOD':'POST',
+                      'CONTENT_TYPE':self.headers['Content-Type'],
+                    })
+
+        # Begin the response
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write('Client: %s\n' % str(self.client_address))
+        self.wfile.write('User-agent: %s\n' % str(self.headers['user-agent']))
+        self.wfile.write('Path: %s\n' % self.path)
+        self.wfile.write('From data:\n')
+
+        # Echo back information about what was posted in the form
+        for field in form.keys():
+            field_item = form[field]
+            if field_item.filename:
+                file_data = field_item.file.read()
+                file_len = len(file_data)
+                del file_data
+                self.wfile.write(
+                        '\tUploaded %s as "%s" (%d bytes)\n' % \
+                                (field, field_item.filename, file_len))
+            else:
+                self.wfile.write('\t%s=%s\n' % (field, form[field].value))
+        return
 
 class GetHandler(BaseHTTPRequestHandler):
     
@@ -34,7 +68,8 @@ class GetHandler(BaseHTTPRequestHandler):
 
 if __name__ == '__main__':
     from BaseHTTPServer import HTTPServer
-    server = HTTPServer(('localhost', 8080), GetHandler)
+    #server = HTTPServer(('localhost', 8080), GetHandler)
+    server = HTTPServer(('localhost', 8080), PostHandler)
     print 'Starting server, use <Ctrl-C> to stop'
     try:
         server.serve_forever()
