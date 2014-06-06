7ce484eff5f26f7770b4409ba0eaa8cfacb45cb8
soarpenguin <soarpenguin@gmail.com>
2014-06-06 23:16:06 +0800

    top.py -- top command implemented use python.

---
+++ top.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+
+# top.py -- top command implemented use python.
+#    get infomation from /proc system.
+#    the main file for system info is:
+#      /proc/stat /proc/<pid>/status /proc/<pid>/stam etc.
+
+import sys
+import re
+from optparse import OptionParser
+
+def read_uptime():
+    try:
+        f_uptime = open("/proc/uptime", "r")
+        line = f_uptime.readline()
+
+        return line.split(None)
+    finally:
+        f_uptime.close()
+
+def get_system_hz():
+    """Return system hz use SC_CLK_TCK."""
+    ticks = os.sysconf(os.sysconf_names['SC_CLK_TCK'])
+
+    if ticks == -1:
+        return 100
+    else:
+        return ticks
+
+def parse_cmdline(argv):
+    """Parses the command-line."""
+
+    # get arguments
+    parser = OptionParser(description='Top command script for display system '
+                                       'infomation.')
+    parser.add_option('-d', '--delay', dest='delay', metavar='int', default=3,
+                      help='Delay time interval as: -d ss.tt (second.tenths).')
+    parser.add_option('-n', '--number', dest='number', metavar='int',
+                      default=0, help='Number of interations limit ad: -n number')
+    parser.add_option('-u', '--user', dest='user', metavar="str",
+                      default="all",
+                      help='Monitor only processes with an effective UID or user name.')
+    parser.add_option('-v', dest='verbose', action='store_true', default=False,
+                      help='Display version information.')
+    (options, args) = parser.parse_args(args=argv[1:])
+
+    return (options, args)
+
+def clrscr():
+    """ Clear screen and move cursor to 1,1 (upper left) pos. """
+    print '\033[2J\033[1;1H'
+
+def clreol():
+    """ Erases from the current cursor position to the end of the current line. """
+    print '\033[K'
+
+def delline():
+    """ Erases the entire current line. """
+    print '\033[2K'
+
+def gotoxy(x, y):
+    """ Moves the cursor to the specified position. """
+    print "\033[%d;%dH" % (x, y)
+
+def main(argv):
+    """ The main top entry point and loop."""
+    
+    options, args = parse_cmdline(argv)
+    clrscr()
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
+
