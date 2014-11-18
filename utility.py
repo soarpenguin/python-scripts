1fb5391e304c72fac61687b95ac93c3fc1d93fcb
soarpenguin <soarpenguin@gmail.com>
2014-11-18 22:38:26 +0800

    add some utility function.

--- utility.py
+++ utility.py
@@ -181,6 +181,14 @@ def mk_boolean(value):
     else:
         return False
 
+def is_quoted(data):
+    return len(data) > 0 and (data[0] == '"' and data[-1] == '"' or data[0] == "'" and data[-1] == "'")
+
+def unquote(data):
+    ''' removes first and last quotes from a string, if the string starts and ends with the same quotes '''
+    if is_quoted(data):
+        return data[1:-1]
+    return data
 
 if __name__ == '__main__':
     #configvalue = getConfig("./config.ini", "mysql", "port")
