888c7fab9ac014e6472b49fb6a2fa60b47bd29ba
soarpenguin <soarpenguin@gmail.com>
2014-11-29 00:46:24 +0800

    rb_tree.py - red black tree for python.

---
+++ rb_tree.py
@@ -0,0 +1,327 @@
+import string
+
+BLACK = 0
+RED = 1
+
+class RBNode(object):
+
+    def __init__(self, key = None, value = None, color = RED):
+        self.left = self.right = self.parent = None
+        self.color = color
+        self.key = key
+        self.value = value
+        self.nonzero = 1
+
+    def __nonzero__(self):
+        return self.nonzero
+
+class RBTree(object):
+
+    def __init__(self, cmpfn=cmp):
+        self.sentinel = RBNode()
+        self.sentinel.left = self.sentinel.right = self.sentinel
+        self.sentinel.color = BLACK
+        self.sentinel.nonzero = 0
+        self.root = self.sentinel
+        self.count = 0
+        # changing the comparison function for an existing tree is dangerous!
+        self.__cmp = cmpfn
+
+    def __len__(self):
+        return self.count
+
+    def rotateLeft(self, x):
+
+        y = x.right
+
+        # establish x.right link
+        x.right = y.left
+        if y.left != self.sentinel:
+            y.left.parent = x
+
+        # establish y.parent link
+        if y != self.sentinel:
+            y.parent = x.parent
+        if x.parent:
+            if x == x.parent.left:
+                x.parent.left = y
+            else:
+                x.parent.right = y
+        else:
+            self.root = y
+
+        # link x and y
+        y.left = x
+        if x != self.sentinel:
+            x.parent = y
+
+    def rotateRight(self, x):
+
+        #***************************
+        #  rotate node x to right
+        #***************************
+
+        y = x.left
+
+        # establish x.left link
+        x.left = y.right
+        if y.right != self.sentinel:
+            y.right.parent = x
+
+        # establish y.parent link
+        if y != self.sentinel:
+            y.parent = x.parent
+        if x.parent:
+            if x == x.parent.right:
+                x.parent.right = y
+            else:
+                x.parent.left = y
+        else:
+            self.root = y
+
+        # link x and y
+        y.right = x
+        if x != self.sentinel:
+            x.parent = y
+
+    def insertFixup(self, x):
+        #************************************
+        #  maintain Red-Black tree balance  *
+        #  after inserting node x           *
+        #************************************
+
+        # check Red-Black properties
+
+        while x != self.root and x.parent.color == RED:
+
+            # we have a violation
+
+            if x.parent == x.parent.parent.left:
+
+                y = x.parent.parent.right
+
+                if y.color == RED:
+                    # uncle is RED
+                    x.parent.color = BLACK
+                    y.color = BLACK
+                    x.parent.parent.color = RED
+                    x = x.parent.parent
+
+                else:
+                    # uncle is BLACK
+                    if x == x.parent.right:
+                        # make x a left child
+                        x = x.parent
+                        self.rotateLeft(x)
+
+                    # recolor and rotate
+                    x.parent.color = BLACK
+                    x.parent.parent.color = RED
+                    self.rotateRight(x.parent.parent)
+            else:
+
+                # mirror image of above code
+
+                y = x.parent.parent.left
+
+                if y.color == RED:
+                    # uncle is RED
+                    x.parent.color = BLACK
+                    y.color = BLACK
+                    x.parent.parent.color = RED
+                    x = x.parent.parent
+
+                else:
+                    # uncle is BLACK
+                    if x == x.parent.left:
+                        x = x.parent
+                        self.rotateRight(x)
+
+                    x.parent.color = BLACK
+                    x.parent.parent.color = RED
+                    self.rotateLeft(x.parent.parent)
+
+        self.root.color = BLACK
+
+    def insertNode(self, key, value):
+        #**********************************************
+        #  allocate node for data and insert in tree  *
+        #**********************************************
+
+        # we aren't interested in the value, we just
+        # want the TypeError raised if appropriate
+        hash(key)
+
+        # find where node belongs
+        current = self.root
+        parent = None
+        while current != self.sentinel:
+            # GJB added comparison function feature
+            # slightly improved by JCG: don't assume that ==
+            # is the same as self.__cmp(..) == 0
+            rc = self.__cmp(key, current.key)
+            if rc == 0:
+                return current
+            parent = current
+            if rc < 0:
+                current = current.left
+            else:
+                current = current.right
+
+        # setup new node
+        x = RBNode(key, value)
+        x.left = x.right = self.sentinel
+        x.parent = parent
+
+        self.count = self.count + 1
+
+        # insert node in tree
+        if parent:
+            if self.__cmp(key, parent.key) < 0:
+                parent.left = x
+            else:
+                parent.right = x
+        else:
+            self.root = x
+
+        self.insertFixup(x)
+        return x
+
+    def deleteFixup(self, x):
+        #************************************
+        #  maintain Red-Black tree balance  *
+        #  after deleting node x            *
+        #************************************
+
+        while x != self.root and x.color == BLACK:
+            if x == x.parent.left:
+                w = x.parent.right
+                if w.color == RED:
+                    w.color = BLACK
+                    x.parent.color = RED
+                    self.rotateLeft(x.parent)
+                    w = x.parent.right
+
+                if w.left.color == BLACK and w.right.color == BLACK:
+                    w.color = RED
+                    x = x.parent
+                else:
+                    if w.right.color == BLACK:
+                        w.left.color = BLACK
+                        w.color = RED
+                        self.rotateRight(w)
+                        w = x.parent.right
+
+                    w.color = x.parent.color
+                    x.parent.color = BLACK
+                    w.right.color = BLACK
+                    self.rotateLeft(x.parent)
+                    x = self.root
+
+            else:
+                w = x.parent.left
+                if w.color == RED:
+                    w.color = BLACK
+                    x.parent.color = RED
+                    self.rotateRight(x.parent)
+                    w = x.parent.left
+
+                if w.right.color == BLACK and w.left.color == BLACK:
+                    w.color = RED
+                    x = x.parent
+                else:
+                    if w.left.color == BLACK:
+                        w.right.color = BLACK
+                        w.color = RED
+                        self.rotateLeft(w)
+                        w = x.parent.left
+
+                    w.color = x.parent.color
+                    x.parent.color = BLACK
+                    w.left.color = BLACK
+                    self.rotateRight(x.parent)
+                    x = self.root
+
+        x.color = BLACK
+
+    def deleteNode(self, z):
+        #****************************
+        #  delete node z from tree  *
+        #****************************
+
+        if not z or z == self.sentinel:
+            return
+
+        if z.left == self.sentinel or z.right == self.sentinel:
+            # y has a self.sentinel node as a child
+            y = z
+        else:
+            # find tree successor with a self.sentinel node as a child
+            y = z.right
+            while y.left != self.sentinel:
+                y = y.left
+
+        # x is y's only child
+        if y.left != self.sentinel:
+            x = y.left
+        else:
+            x = y.right
+
+        # remove y from the parent chain
+        x.parent = y.parent
+        if y.parent:
+            if y == y.parent.left:
+                y.parent.left = x
+            else:
+                y.parent.right = x
+        else:
+            self.root = x
+
+        if y != z:
+            z.key = y.key
+            z.value = y.value
+
+        if y.color == BLACK:
+            self.deleteFixup(x)
+
+        del y
+        self.count = self.count - 1
+
+    def findNode(self, key):
+        #******************************
+        #  find node containing data
+        #******************************
+
+        # we aren't interested in the value, we just
+        # want the TypeError raised if appropriate
+        hash(key)
+
+        current = self.root
+
+        while current != self.sentinel:
+            # GJB added comparison function feature
+            # slightly improved by JCG: don't assume that ==
+            # is the same as self.__cmp(..) == 0
+            rc = self.__cmp(key, current.key)
+            if rc == 0:
+                return current
+            else:
+                if rc < 0:
+                    current = current.left
+                else:
+                    current = current.right
+
+        return None
+
+    def firstNode(self):
+        cur = self.root
+        while cur.left:
+            cur = cur.left
+        return cur
+
+    def lastNode(self):
+        cur = self.root
+        while cur.right:
+            cur = cur.right
+        return cur
+
