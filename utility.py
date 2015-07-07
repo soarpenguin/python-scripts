014819090d1c8728b86e06547f4a0b70a7bd7758
soarpenguin <soarpenguin@gmail.com>
2015-07-07 23:50:55 +0800

    add run_cmd in utility.py

--- utility.py
+++ utility.py
@@ -12,6 +12,8 @@ from stat import S_ISDIR, S_ISREG, ST_MODE
 import terminal
 import pipes
 import stat
+import shlex
+import select
 
 try:
     import json
@@ -106,6 +108,43 @@ def runcmd(cmd):
     else:
         print(stdout, stderr)
         return False
+
+
+def run_cmd(cmd, live=False, readsize=10):
+
+    #readsize = 10
+    cmdargs = shlex.split(cmd)
+    p = subprocess.Popen(cmdargs, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    stdout = ''
+    stderr = ''
+    rpipes = [p.stdout, p.stderr]
+    while True:
+        rfd, wfd, efd = select.select(rpipes, [], rpipes, 1)
+
+        if p.stdout in rfd:
+            dat = os.read(p.stdout.fileno(), readsize)
+            if live:
+                sys.stdout.write(dat)
+            stdout += dat
+            if dat == '':
+                rpipes.remove(p.stdout)
+        if p.stderr in rfd:
+            dat = os.read(p.stderr.fileno(), readsize)
+            stderr += dat
+            if live:
+                sys.stdout.write(dat)
+            if dat == '':
+                rpipes.remove(p.stderr)
+        # only break out if we've emptied the pipes, or there is nothing to
+        # read from and the process has finished.
+        if (not rpipes or not rfd) and p.poll() is not None:
+            break
+        # Calling wait while there are still pipes to read can cause a lock
+        elif not rpipes and p.poll() == None:
+            p.wait()
+
+    return p.returncode, stdout, stderr
 
 def getConfig(file, group, configName):
     config = ConfigParser.ConfigParser()
