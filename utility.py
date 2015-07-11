734f4dd8e664e7efb2c0b599a37bd8cf6de653ff
soarpenguin <soarpenguin@gmail.com>
2015-07-11 23:36:34 +0800

    format a string to json mode.

--- utility.py
+++ utility.py
@@ -557,6 +557,25 @@ def json_format_dict(data, pretty=False):
     else:
         return json.dumps(data)
 
+def jsonify(result, format=False):
+    ''' format JSON output (uncompressed or uncompressed) '''
+
+    if result is None:
+        return "{}"
+    result2 = result.copy()
+    for key, value in result2.items():
+        if type(value) is str:
+            result2[key] = value.decode('utf-8', 'ignore')
+
+    indent = None
+    if format:
+        indent = 4
+
+    try:
+        return json.dumps(result2, sort_keys=True, indent=indent, ensure_ascii=False)
+    except UnicodeDecodeError:
+        return json.dumps(result2, sort_keys=True, indent=indent)
+
 if __name__ == '__main__':
     #configvalue = getConfig("./config.ini", "mysql", "port")
     #print configvalue
