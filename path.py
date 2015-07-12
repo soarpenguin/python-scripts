a4990bc84c447af00a4a59de03e5582484eae0f4
soarpenguin <soarpenguin@gmail.com>
2015-07-12 23:14:00 +0800

    add utility func for prepare writeable dir.

--- path.py
+++ path.py
@@ -20,6 +20,29 @@ def unfrackpath(path):
     '''
     return os.path.normpath(os.path.realpath(os.path.expandvars(os.path.expanduser(path))))
 
+def prepare_writeable_dir(tree, mode=0777):
+    ''' make sure a directory exists and is writeable '''
+
+    # modify the mode to ensure the owner at least
+    # has read/write access to this directory
+    mode |= 0700
+
+    # make sure the tree path is always expanded
+    # and normalized and free of symlinks
+    tree = unfrackpath(tree)
+
+    if not os.path.exists(tree):
+        try:
+            os.makedirs(tree, mode)
+        except (IOError, OSError), e:
+            print("Could not make dir %s: %s" % (tree, e))
+            sys.exit(1)
+    if not os.access(tree, os.W_OK):
+        print("Cannot write to path %s" % tree)
+        sys.exit(1)
+    return tree
+
+
 def makedirs_safe(path, mode=None):
     '''Safe way to create dirs in muliprocess/thread environments'''
     if not os.path.exists(path):
